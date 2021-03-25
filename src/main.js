a275b57386c241241db482e9a71342bdce0b5d12
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-03-25 00:17:14 -0300

    validacion-de-fecha
    Validando que la fecha no este  vacía y si es así, mostrar el mensaje de error.

--- src/main.js
+++ src/main.js
@@ -7,18 +7,21 @@ buttton.onclick = function (event) {
     let resultadoHeaderDate = document.querySelector(`.strong-date`);
     let resultadoHeaderBase = document.querySelector(`.strong-base`);
     let resultadoUl = document.querySelector(`.resultado-ul`)
-    
+
+    validarFecha();
+
     if (date != ``) {
+        borrarErroresMostrados();
         fetch(`https://api.exchangeratesapi.io/${date}?base=${base}`)
             .then(respuesta => respuesta.json())
             .then(respuestaJSON => {
 
-                Object.keys(respuestaJSON.rates).forEach(moneda=>{
+                Object.keys(respuestaJSON.rates).forEach(moneda => {
                     let newLi = document.createElement(`li`)
                     newLi.className = `moneda-li`;
                     newLi.textContent = `${moneda}: ${respuestaJSON.rates[moneda]}`;
                     resultadoUl.appendChild(newLi);
-                
+
                 })
             });
 
@@ -27,17 +30,42 @@ buttton.onclick = function (event) {
         mostrarResultado();
     }
 
-    
+    event.preventDefault()
 
-}
-function mostrarResultado(){
+};
+function mostrarResultado() {
     let resultado = document.querySelector(`#resultado`)
     resultado.className = `resultado`;
+};
+function ocultarResultado() {
+    let resultado = document.querySelector(`#resultado`);
+    resultado.className = `oculto`;
 }
 
-function resetearResultado(){
+function resetearResultado() {
     let liElements = document.querySelectorAll(`.moneda-li`)
-    liElements.forEach(li=>{
+    liElements.forEach(li => {
         li.remove();
     })
+};
+
+/* Seccion Errores */
+function validarFecha() {
+    let date = document.querySelector(`#date`).value;
+    if (date === ``) {
+        ocultarResultado();
+        mostrarErrores();
+    }
+};
+function borrarErroresMostrados() {
+    let inputError = document.querySelector(`#date`);
+    inputError.style.border = ``;
+    let cartelError = document.querySelector(`#mostrar-errores`);
+    cartelError.className = `oculto`;
+};
+function mostrarErrores() {
+    let inputDate = document.querySelector(`#date`);
+    inputDate.style.border = `2px solid #f70c47`;
+    let cartelError = document.querySelector(`#mostrar-errores`);
+    cartelError.className = `form-row__errores`;
 };
