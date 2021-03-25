36d3553d6865d66ff521e2becc7b1c40584b1ed9
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-03-24 23:14:08 -0300

    logica-tasa-de-cambio
    Creando la lógica para esta interfaz, que modificara un header donde dice la fecha y la base que se solicito para luego crear una lista con los datos solicitados. posteriormente borrando si existieran datos antes ya solicitados para mostrar los nuevos.

---
+++ src/main.js
@@ -0,0 +1,43 @@
+
+let buttton = document.querySelector(`#consultar-tasas`);
+buttton.onclick = function (event) {
+    resetearResultado();
+    let date = document.querySelector(`#date`).value;
+    let base = document.querySelector(`#base`).value;
+    let resultadoHeaderDate = document.querySelector(`.strong-date`);
+    let resultadoHeaderBase = document.querySelector(`.strong-base`);
+    let resultadoUl = document.querySelector(`.resultado-ul`)
+    
+    if (date != ``) {
+        fetch(`https://api.exchangeratesapi.io/${date}?base=${base}`)
+            .then(respuesta => respuesta.json())
+            .then(respuestaJSON => {
+
+                Object.keys(respuestaJSON.rates).forEach(moneda=>{
+                    let newLi = document.createElement(`li`)
+                    newLi.className = `moneda-li`;
+                    newLi.textContent = `${moneda}: ${respuestaJSON.rates[moneda]}`;
+                    resultadoUl.appendChild(newLi);
+                
+                })
+            });
+
+        resultadoHeaderDate.textContent = date;
+        resultadoHeaderBase.textContent = base;
+        mostrarResultado();
+    }
+
+
+
+}
+function mostrarResultado(){
+    let resultado = document.querySelector(`#resultado`)
+    resultado.className = `resultado`;
+}
+
+function resetearResultado(){
+    let liElements = document.querySelectorAll(`.moneda-li`)
+    liElements.forEach(li=>{
+        li.remove();
+    })
+};
