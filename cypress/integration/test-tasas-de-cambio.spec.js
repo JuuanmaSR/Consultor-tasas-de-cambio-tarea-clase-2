f98a7e0538cde051fd866c2580bcd8fb176711fa
JuuanmaSR <juan_rider_sp@hotmail.com>
2021-03-27 20:45:32 -0300

    test-cypress
    testeando con cypress  el código para encontrar errores.

--- cypress/integration/test-tasas-de-cambio.spec.js
+++ cypress/integration/test-tasas-de-cambio.spec.js
@@ -1,10 +1,27 @@
-<reference types="Cypress"/>
+
 describe(`Testeando tarea clase 12`,() => {
     before(()=>{
-        cy.visit(`http://192.168.1.2:8080/index.html`);
+        cy.visit(`http://192.168.1.2:8080`);
     });
+
     it('Testeando validacion de fecha', () => {
-        cy.get(`#`)
+        cy.get(`#consultar-tasas`).click();
+        cy.get(`#mostrar-errores`).should(`be.visible`);
+        cy.get(`#resultado`).should(`not.visible`);
     });
     
-})
+    it('Testando ingreso de fecha correcto', () => {
+        cy.get(`#date`).type(`2021-03-27`);
+        cy.get(`#base`).type(`USD`);
+        cy.get(`#consultar-tasas`).click();
+        cy.get(`#mostrar-errores`).should(`not.visible`)    
+    });
+
+    it('Comprobando que se muestran los resultados', () => {
+        cy.get(`#resultado`).should(`be.visible`);
+        cy.get(`.strong-date`).should(`have.text`, `2021-03-27`);
+        cy.get(`.strong-base`).should(`have.text`, `USD`);
+        cy.get(`.resultado-ul li`).should(`have.length`, `33`);
+    });
+
+});
